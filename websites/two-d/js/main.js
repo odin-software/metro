b09f702dd0f712dec2e6ad10e9508a0607cfb7f1
Kendry Grullon <kengrullon@gmail.com>
2024-02-23 13:45:21 -0400

    Added the roads for the experiment.

--- websites/two-d/js/main.js
+++ websites/two-d/js/main.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -10,9 +11,17 @@ function animate() {
   // Resizing the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   canvas.height = window.innerHeight;
+  car.update();
+
+  ctx.save(); 
+  ctx.translate(0, -car.y + canvas.height *0.7);
+
+  // Road draw
+  road.draw(ctx);
 
   // Car movement and draw
-  car.update();
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
